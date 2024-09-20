d4a208e03551d16e650f25e3cae46f0a5f233cb8
gkphp <gautam.pandey@mewurk.com>
2024-09-20 13:22:10 +0530

    bug fix

--- models/student.js
+++ models/student.js
@@ -1,27 +1,30 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
-const studentSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true,
+// this is the model for students
+const studentSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    dateOfBirth: {
+      type: Date,
+      required: true,
+    },
   },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
-  dateOfBirth: {
-    type: Date,
-    required: true,
-  },
-}, {timestamps: true});
+  { timestamps: true }
+);
 
 const Student = mongoose.model("Student", studentSchema);
 
